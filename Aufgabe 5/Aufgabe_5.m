6acc192bdb879b92716a3ecf978fbbb92df7505a
Milan larsen <milanlarsen@gmail.com>
2022-06-15 18:25:04 +0200

    Aufgabe 5.2
    Fertig

--- Aufgabe 5/Aufgabe_5.m
+++ Aufgabe 5/Aufgabe_5.m
@@ -29,6 +29,30 @@ harmonic2=[mag(126);mag(251);mag(501)]
 frequencys2=[mag(1)*fs/N;125*fs/N;250*fs/N;500*fs/N]
 title('N=1k Abtastwerte')
 % Erklärung TODO
+% Durch die Verringerung der Abtastwerte erhöht sich der Einfluss der seitlichen unstätigkeiten. Ebenso   
 %%
 % 5.3 Saegezahn 
+clear all
+fs=100;
+t=0:1/fs:8.2;
+f=1;
+a=pi/2;
+sins=zeros(20,821);
+for i=1:20
+sins(i,:)=(-1)^i*sin(2*pi*i*f*t)/i;
+saw=2*a/pi*sum(sins);
+pause(0.5)
+plot(t,saw)
+xlabel('t [s]')
+ylabel('Amplitude')
+grid on;
+end
+%%
+figure
+plot(t,saw+1)
+grid on
+xlabel('t [s]')
+ylabel('Amplitude')
 
+%%
+writematrix(saw,'saegezahn.csv')
